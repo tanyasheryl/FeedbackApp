de7c5d83675b64f76db681c6fdcb7f27deb7dbdf
Sheryl <TS051082@cerner.net>
2018-03-20 15:47:25 +0530

    done authflow

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const app = express();
+//const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+const passportConfig = require('./services/passport');
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+mongoose.connect(keys.mongoURI, function(err) {
+  if (err) console.log('error', err);
 });
-
+const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
